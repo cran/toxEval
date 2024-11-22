d1c02ba9bd3d397b22b9e4237fa9597a50468596
Laura DeCicco <ldecicco@usgs.gov>
2024-11-22 21:20:02 +0000

    version 1.4.0

--- inst/doc/Introduction.R
+++ inst/doc/Introduction.R
@@ -1,15 +1,15 @@
 ## ----end_point_info, eval=FALSE-----------------------------------------------
-#  library(toxEval)
-#  end_point_info <- end_point_info
+# library(toxEval)
+# end_point_info <- end_point_info
 
 ## ----eval=FALSE---------------------------------------------------------------
-#  filtered_ep <- filter_groups(end_point_info,
-#                groupCol = "intended_target_family",
-#                assays = c("ATG","NVS", "OT", "TOX21",
-#                           "CEETOX", "APR", "CLD", "TANGUAY",
-#                           "NHEERL_PADILLA","NCCT_SIMMONS", "ACEA"),
-#                remove_groups = c("Background Measurement",
-#                                  "Undefined"))
+# filtered_ep <- filter_groups(end_point_info,
+#               groupCol = "intended_target_family",
+#               assays = c("ATG","NVS", "OT", "TOX21",
+#                          "CEETOX", "APR", "CLD", "TANGUAY",
+#                          "NHEERL_PADILLA","NCCT_SIMMONS", "ACEA"),
+#               remove_groups = c("Background Measurement",
+#                                 "Undefined"))
 
 ## ----eval=TRUE----------------------------------------------------------------
 citation(package = "toxEval")
--- inst/doc/PrepareData.R
+++ inst/doc/PrepareData.R
@@ -4,3 +4,35 @@ knitr::include_graphics("tabs.png")
 ## ----updateIMAGE, echo=FALSE--------------------------------------------------
 knitr::include_graphics("data.png")
 
+## -----------------------------------------------------------------------------
+library(toxEval)
+library(ggplot2)
+
+CAS <- c("1912-24-9")
+default_id = c(5, 6, 11, 15, 18)
+
+ACC_atrazine <- get_ACC(CAS) |> 
+  dplyr::arrange(ACC_value) |> 
+  dplyr::mutate(index = dplyr::row_number(),
+                percent = 100*index/dplyr::n(),
+                has_default_flag = colSums(sapply(flags, "%in%", x = default_id)) > 0)
+
+ggplot() +
+  geom_point(data = ACC_atrazine,
+             aes(x = ACC_value, y = percent,
+                 color = has_default_flag)) +
+  xlab("ACC ug/L") +
+  ylab("Percentile") +
+  ggtitle("Individual ACC values for Atrazine") +
+  geom_text(data = head(ACC_atrazine |> 
+                          dplyr::filter(!has_default_flag),
+                        n = 5),
+            aes(y = percent, x = ACC_value,
+                label = endPoint,
+                color = has_default_flag), 
+             size = 2, hjust = -0.2, show.legend = FALSE) +
+  theme_bw() +
+  scale_color_manual("Includes default\nflags for removal", 
+                     values = c("blue", "grey80"))
+
+
